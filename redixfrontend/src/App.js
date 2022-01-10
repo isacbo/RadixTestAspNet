bbb823c3bbc72929e453a85b8f480c43bffde840
isacbo <88782422+isacbo@users.noreply.github.com>
2022-01-10 15:27:09 +0100

    init

---
+++ redixfrontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function App() {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    console.log("Hello there");
+    fetch("http://localhost/WeatherForecast").then( res => res.json()).then(data => setState(
+      JSON.stringify( data)
+      ));
+  }, []);
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+      {state}
+    </div>
+  );
+}
+
+export default App;
